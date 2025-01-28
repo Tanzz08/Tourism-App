b6c0790e9aece4d4fa655eb66db94d6cb7422aa2
tanzz <bustanuldarking23@gmail.com>
2025-01-28 13:39:37 +0800

    mengubha flow menjadi livedata

--- app/src/main/java/com/dicoding/tourismapp/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/dicoding/tourismapp/favorite/FavoriteViewModel.kt
@@ -1,14 +1,15 @@
 package com.dicoding.tourismapp.favorite
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.toLiveData
 import com.dicoding.tourismapp.core.data.TourismRepository
 import com.dicoding.tourismapp.core.domain.usecase.TourismUseCase
 
 class FavoriteViewModel(tourismUseCase: TourismUseCase) : ViewModel() {
 
-    // pada viewmodel convert data Flowable menjadi LiveData menggunakan LiveDataReactiveStreams
-    val favoriteTourism = tourismUseCase.getFavoriteTourism().toLiveData()
+    // pada viewmodel convert data Flow menjadi LiveData
+    val favoriteTourism = tourismUseCase.getFavoriteTourism().asLiveData()
 
 }
 
--- app/src/main/java/com/dicoding/tourismapp/home/HomeViewModel.kt
+++ app/src/main/java/com/dicoding/tourismapp/home/HomeViewModel.kt
@@ -1,14 +1,15 @@
 package com.dicoding.tourismapp.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.toLiveData
 import com.dicoding.tourismapp.core.data.TourismRepository
 import com.dicoding.tourismapp.core.domain.usecase.TourismUseCase
 
 class HomeViewModel(tourismUseCase: TourismUseCase) : ViewModel() {
 
-    // pada viewmodel convert data Flowable menjadi LiveData menggunakan LiveDataReactiveStreams
-    val tourism = tourismUseCase.getAllTourism().toLiveData()
+    // pada viewmodel convert data Flow menjadi LiveData
+    val tourism = tourismUseCase.getAllTourism().asLiveData()
 
 }
 
