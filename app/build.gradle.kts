52b7d61ae305ba5e0f577fb06f21e6423446f1d6
tanzz <bustanuldarking23@gmail.com>
2025-01-29 11:31:43 +0800

    add koin dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,4 +75,7 @@ dependencies {
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.lifecycle.livedata.ktx)
+
+    // koin
+    implementation(libs.koin.android)
 }
